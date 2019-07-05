94e828c4e3566b9fbcc56c175e13349ec109af98
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-04 19:26:39 -0700

    cleaned up js, text still won't DOM and click function not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,45 @@
-//set vars
-//generate value of gems
-//assign value of gems
-//set total order price
-//determine if gem total is higher or lower than order total
-//update wins/losses
-//reset game based on wins/losses
-//reset value of gems and order total
-
 var win = 0;
-$("#wins").text(wins);
-
 var loss = 0;
-$("#loss").text(loss);
 
 var orderTot = Math.floor((Math.random()*120)+19);
-$("#random").text(orderTot);
+$("#random").html("<h2>The random order total is: </h2>" + orderTot);
+
 var custTot= 0;
 
 var wooder =Math.floor((Math.random() * 12) + 1);
 var pretzel=Math.floor((Math.random() * 12) + 1);
 var tasty = Math.floor((Math.random() * 12) + 1);
 var cheese=Math.floor((Math.random() * 12) + 1);
+
+
+function reset(){
+wooder=wooder;
+pretzel=pretzel;
+tasty=tasty;
+cheese=cheese;
+orderTot=orderTot;
+}
+
+$(document).ready(function() {
+   $("img").on("click", function(){
+    custTot+=this;
+if (custTot===orderTot){
+    alert("You win!");
+    wins++;
+    $("#wins").text(wins);
+    reset();
+} else if (custTot>= orderTot){
+    alert("You lose!!");
+    losses++;
+    $("#loss").text(loss);
+    reset();
+} else ("img").on("click", function() {
+    custTot+=this;
+        });
+    });
+});
+
+
 
 // var menuValue = [];
 //     for (var i = 0; i < 4; i++)
@@ -35,58 +54,3 @@ var cheese=Math.floor((Math.random() * 12) + 1);
 // menuItems.addClass("menuitems");
 // menuItems.attr("menu-value", menuValue[i]);
  
-$(document).ready ( function () {
-   $(".menuitems").on("click", function(){
-if (custTot===orderTot){
-    alert("You win!");
-    wins++;
-} else if (custTot>= orderTot);
-    alert("You lose!!");
-    losses++;
-} ("#wooder"||"#pretzel"||"#tasty"||"#cheese").on("click", function() {
-    custTot+=this;
-    });
-
-
-console.log(custTot);
-// $(document).ready ( function () {
-//     $(".menuitems").on("click", function() {
-
-//     //    var menuValue = ($(this).attr("menu-value"));
-//     //    menuValue=parseInt(menuValue);
-//    wooder.attr("value", wooderVal);
-//         console.log("wodder value after click is:  "+ wooderVal);
-//     });
-
-//     $("wooder")
- 
-// });
-
-// console.log("menu value is "+ menuValue);
-// console.log("customer order total is " + custTot);
-// console.log("wins are "+ win);
-// console.log("lossess are " + loss);
-// console.log("The order Total is " + orderTot);
-// console.log("The menu item costs are " + menuValue);
-
-
-
-// if (custTot===orderTot){
-//     wins++;
-//     reset();
-// }else if (custTot>orderTot){
-//     losses++;
-//     reset();
-
-// };
-// });
-
-// var reset = function(){
-//     //reset order total and menu item values
-// }
-
-
-// $(".orderTot").text("The random order total is: "+ orderTot);
-// $(".scoreboard").text("Wins: "+ win, "Losses: "+ loss);
-// $(".custTot").text("Your order total is: "+ custTot);
-
