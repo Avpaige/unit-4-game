69160d86ff41393d25c34629bf15ccbb36b56226
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-04 19:01:13 -0700

    different approach to js for item values and calculating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,62 +5,88 @@
 //determine if gem total is higher or lower than order total
 //update wins/losses
 //reset game based on wins/losses
-    //reset value of gems and order total
-
-
+//reset value of gems and order total
 
 var win = 0;
+$("#wins").text(wins);
+
 var loss = 0;
+$("#loss").text(loss);
+
 var orderTot = Math.floor((Math.random()*120)+19);
+$("#random").text(orderTot);
 var custTot= 0;
 
-var menuValue = [];
-    for (var i = 0; i < 4; i++)
-    {menuValue.push(Math.floor(Math.random() * 12) + 1)
-        }var str = "";
-    for (var i = 0; i < 4; i++)
-    {str += "," + menuValue[i];
-    }
+var wooder =Math.floor((Math.random() * 12) + 1);
+var pretzel=Math.floor((Math.random() * 12) + 1);
+var tasty = Math.floor((Math.random() * 12) + 1);
+var cheese=Math.floor((Math.random() * 12) + 1);
 
+// var menuValue = [];
+//     for (var i = 0; i < 4; i++)
+//     {menuValue.push(Math.floor(Math.random() * 12) + 1)
+//         }var str = "";
+//     for (var i = 0; i < 4; i++)
+//     {str += "," + menuValue[i];
 
-$(".orderTot").text("The random order total is: "+ orderTot);
-$(".scoreboard").text("Wins: "+ win, "Losses: "+ loss);
+// }
 
-
-var menuItems = $("img");
-menuItems.attr("menuvalue", menuValue[i]);
-
-var reset = function(){
-    //reset order total and menu item values
-}
-
-
-console.log("The order Total is " + orderTot);
-console.log("The menu item costs are " + menuValue);
-
-$(".menuitems").on("click", function(){
-
-    alert("click listener is working");
-    
-var menuValue = ($(this).attr("menuvalue"));
-var menuValue=parseInt(menuValue);
-menuValue+=custTot;
-
-$(".custTot").text("Your order total is: "+ custTot);
-
-
-
+// var menuItems= $("img");
+// menuItems.addClass("menuitems");
+// menuItems.attr("menu-value", menuValue[i]);
+ 
+$(document).ready ( function () {
+   $(".menuitems").on("click", function(){
 if (custTot===orderTot){
+    alert("You win!");
     wins++;
-    reset();
-}else if (custTot>orderTot){
+} else if (custTot>= orderTot);
+    alert("You lose!!");
     losses++;
-    reset();
+} ("#wooder"||"#pretzel"||"#tasty"||"#cheese").on("click", function() {
+    custTot+=this;
+    });
 
-}
-});
 
-console.log("menu value is "+ menuValue);
-console.log("customer order total is " + custTot);
-console.log("wins are "+ win);
-console.log("lossess are " + loss);
+console.log(custTot);
+// $(document).ready ( function () {
+//     $(".menuitems").on("click", function() {
+
+//     //    var menuValue = ($(this).attr("menu-value"));
+//     //    menuValue=parseInt(menuValue);
+//    wooder.attr("value", wooderVal);
+//         console.log("wodder value after click is:  "+ wooderVal);
+//     });
+
+//     $("wooder")
+ 
+// });
+
+// console.log("menu value is "+ menuValue);
+// console.log("customer order total is " + custTot);
+// console.log("wins are "+ win);
+// console.log("lossess are " + loss);
+// console.log("The order Total is " + orderTot);
+// console.log("The menu item costs are " + menuValue);
+
+
+
+// if (custTot===orderTot){
+//     wins++;
+//     reset();
+// }else if (custTot>orderTot){
+//     losses++;
+//     reset();
+
+// };
+// });
+
+// var reset = function(){
+//     //reset order total and menu item values
+// }
+
+
+// $(".orderTot").text("The random order total is: "+ orderTot);
+// $(".scoreboard").text("Wins: "+ win, "Losses: "+ loss);
+// $(".custTot").text("Your order total is: "+ custTot);
+
