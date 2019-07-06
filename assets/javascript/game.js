062029f00caf0e295fb2366e83a458bba50c3943
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-05 22:16:23 -0700

    fixed scoreboard, css and win/loss trigger

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,41 @@
 var win = 0;
 var loss = 0;
 
-var orderTot = Math.floor((Math.random()*120)+19);
+var orderTot = Math.floor((Math.random()*120-19+1)+19);
 $("#random").html(orderTot);
 
 var custTot= 0;
 
-var wooder =Math.floor((Math.random() * 12) + 1);
-var pretzel=Math.floor((Math.random() * 12) + 1);
-var tasty = Math.floor((Math.random() * 12) + 1);
-var cheese=Math.floor((Math.random() * 12) + 1);
+var wooder =Math.floor((Math.random() * 12-1+1) + 1);
+var pretzel=Math.floor((Math.random() * 12-1+1) + 1);
+var tasty = Math.floor((Math.random() * 12-1+1) + 1);
+var cheese=Math.floor((Math.random() * 12-1+1) + 1);
 
 function reset(){
-wooder=wooder;
-pretzel=pretzel;
-tasty=tasty;
-cheese=cheese;
-orderTot=orderTot;
+wooder=Math.floor((Math.random() * 12) + 1);
+pretzel=Math.floor((Math.random() * 12) + 1);
+tasty=Math.floor((Math.random() * 12) + 1);
+cheese=Math.floor((Math.random() * 12-1+1) + 1);
+orderTot=Math.floor((Math.random()*120-19+1)+19);
+$("#random").html(orderTot);
+custTot= 0;
+$("#receipt").html(custTot);
 }
 
 $(document).ready(function() {
    $(".menuitems").on("click", function(){
+    $("#random").html(orderTot);
 if (custTot===orderTot){
+        reset();
         alert("You win, your order is free!");
-        wins++;
-        $("#wins").html(wins);
+        win++;
+        $("#wins").html(win);
         reset();
 } else if (custTot>= orderTot){
-        alert("You lost!");
-        losses++;
+        reset(); 
+        alert("You lost! Hope you brought cash!");
+        loss++;
         $("#loss").html(loss);
-        reset();
 } else if($("#wooder")) {
     custTot= wooder + custTot;
     $("#receipt").html(custTot);
